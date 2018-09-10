f57068e2406d98e196ca2cae010db2d58bbd59b9
Dev-Marky <mark@healthwatch.se>
2018-09-10 17:07:26 +0800

    Modified to apply AdminLTE template.

---
+++ SaI/SaI/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

2d46945fddc1bf09d491196c3d0de540faca7ef0
bncast <bncast@gmail.com>
2018-09-12 19:15:58 +0800

    Added department module, unfinished no delete

---
+++ SaI/SaI/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaI.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public string BackColor { get; set; }
+    }
+}
